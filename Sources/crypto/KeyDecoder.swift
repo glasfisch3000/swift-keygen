24426b60d7cd830073c462a62b27d736bb030707
glasfisch3000 <jakob@danckwerts.net>
2024-05-03 00:46:01 +0200

    fixed typo

--- Sources/crypto/KeyDecoder.swift
+++ Sources/crypto/KeyDecoder.swift
@@ -2,6 +2,6 @@ import Foundation
 
 /// An object that can be used to decode cryptographic keys from a specific format.
 public protocol KeyDecoder {
-    /// Decodes a key from raw data`.
+    /// Decodes a key from raw data.
     func decodeKey<Key: CryptoKey>(from data: Data, as type: Key.Type) throws -> Key
 }
