120b3b4929b50ee2f6304abdbae67ee200764cd8
glasfisch3000 <jakob@danckwerts.net>
2024-05-02 15:49:46 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-keygen",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    ],
+    targets: [
+        
+    ]
+)
---
+++ Sources/swift_keygen.swift
@@ -0,0 +1,8 @@
+import ArgumentParser
+
+@main
+struct swift_keygen: ParsableCommand {
+    mutating func run() throws {
+        print("Hello, world!")
+    }
+}
