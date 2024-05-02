0bf13f8acf610f198f6fc8c82e13f1f5741bb1f2
glasfisch3000 <jakob@danckwerts.net>
2024-05-02 16:23:02 +0200

    added executable target and product

--- Package.swift
+++ Package.swift
@@ -5,11 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "swift-keygen",
+    products: [
+        .executable(name: "swift-keygen", targets: ["keygen"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.3.0")
     ],
     targets: [
-        
+        .executableTarget(
+            name: "keygen",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "Crypto", package: "swift-crypto"),
+            ],
+            path: "./Sources/"
+        )
     ]
 )
