b42312016a7f0e64c1c70d4fcb2f3202050f1401
glasfisch3000 <jakob@danckwerts.net>
2024-05-03 00:41:22 +0200

    updated package target platform version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-keygen",
     platforms: [
-        .macOS(.v10_15)
+        .macOS("13")
     ],
     products: [
         .executable(name: "swift-keygen", targets: ["keygen"])
