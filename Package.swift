95482e8ea7610e2a448f867ef4ddf9547a01f9bb
glasfisch3000 <jakob@danckwerts.net>
2024-05-02 17:10:17 +0200

    added macOS target platform details

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "swift-keygen",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     products: [
         .executable(name: "swift-keygen", targets: ["keygen"])
     ],
