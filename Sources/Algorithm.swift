08042cf9cf37b091f5293e3f640ab26f70cd2bff
glasfisch3000 <jakob@danckwerts.net>
2024-05-02 17:14:22 +0200

    added Algorithm.generateKey

--- Sources/Algorithm.swift
+++ Sources/Algorithm.swift
@@ -16,3 +16,11 @@ extension Algorithm {
         }
     }
 }
+
+extension Algorithm {
+    func generateKey() -> any PrivateKey {
+        switch self {
+        case .ed25519: keygen_ed25519()
+        }
+    }
+}
