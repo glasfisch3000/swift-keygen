fdaf101b82713018d6aff6ffbb4c564931d6e48e
glasfisch3000 <jakob@danckwerts.net>
2024-05-02 17:12:36 +0200

    added key protocols

---
+++ Sources/key-protocols.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public protocol CryptoKey: RawRepresentable where RawValue: DataProtocol {
+    var rawRepresentation: RawValue { get }
+    init(rawRepresentation: RawValue) throws
+}
+
+public protocol PrivateKey: CryptoKey {
+    associatedtype PublicKey: CryptoKey
+    
+    func makePublicKey() -> PublicKey
+}
+
+
+extension CryptoKey {
+    public var rawValue: RawValue { rawRepresentation }
+    
+    public init?(rawValue: RawValue) {
+        try? self.init(rawRepresentation: rawValue)
+    }
+}
