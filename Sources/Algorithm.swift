3ccb416e6e50d711115b304bffa80680fcaac1e8
glasfisch3000 <jakob@danckwerts.net>
2024-05-02 17:10:55 +0200

    removed rsa algorithm

--- Sources/Algorithm.swift
+++ Sources/Algorithm.swift
@@ -1,15 +1,16 @@
-enum Algorithm: String {
-    case rsa
+import ArgumentParser
+
+public enum Algorithm: String, CaseIterable {
     case ed25519
 }
 
 extension Algorithm: Hashable { }
 extension Algorithm: Codable { }
+extension Algorithm: ExpressibleByArgument { }
 
 extension Algorithm {
-    init?(rawValue: String) {
+    public init?(rawValue: String) {
         switch rawValue.lowercased() {
-        case "rsa": self = .rsa
         case "ec25519", "ed25519", "25519": self = .ed25519
         default: return nil
         }
